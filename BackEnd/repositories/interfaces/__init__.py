b4a0069434efa97d33a689e645bc6304b124bbd8
JUAN MANUEL USUGA GALEANO <juanusuga1113848@correo.itm.edu.co>
2025-11-20 21:51:30 -0500

    Merge completado del main

--- BackEnd/repositories/interfaces/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Interfaces para repositories (Dependency Inversion Principle - SOLID)"""
-
-from .medicamento_repository import IMedicamentoRepository
-from .movimiento_repository import IMovimientoRepository
-
-__all__ = [
-    'IMedicamentoRepository',
-    'IMovimientoRepository',
-]
